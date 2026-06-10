f534a1740908f7e9c9d486c48eea257ed99cc7e9
Quincy-Bee <quincybee@icloud.com>
2026-06-09 22:36:06 -0400

    javascript file added

---
+++ index.js
@@ -0,0 +1,4 @@
+//get elements fro pokémon API
+const input = document.querySelector("#pokemonInput");
+const btn = document.querySelector("#searchBtn");
+const result = document.querySelector("#result");
